daa0148b7a820e092ac2bde9e75d55744d045f97
joelsherrill <joelsherrill>
2009-04-22 21:13:25 +0000

    2009-04-22	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* src/hweeprom.h: Fix inconsitent formatting.

--- src/hweeprom.h
+++ src/hweeprom.h
@@ -2,8 +2,8 @@
  ****************************************************************************
  *
  * simulavr - A simulator for the Atmel AVR family of microcontrollers.
- * Copyright (C) 2001, 2002, 2003   Klaus Rudolph		
- * 
+ * Copyright (C) 2001, 2002, 2003   Klaus Rudolph
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
@@ -31,44 +31,44 @@
 #include "memory.h"
 
 class HWEeprom: public Hardware, public Memory {
-	protected:
-		unsigned int eear;
-		unsigned char eecr;
-		unsigned char eedr;
+    protected:
+        unsigned int eear;
+        unsigned char eecr;
+        unsigned char eedr;
 
-		unsigned int cpuHoldCycles;
-		unsigned int uSleepForOutput;
-		unsigned int state;
-		unsigned int writeEnableCycles;	//let EEMWE stay only 4 cycles
-		unsigned int writeStartTime;
+        unsigned int cpuHoldCycles;
+        unsigned int uSleepForOutput;
+        unsigned int state;
+        unsigned int writeEnableCycles;    //let EEMWE stay only 4 cycles
+        unsigned int writeStartTime;
         AvrDevice *core;
 
-	public:
-		HWEeprom(AvrDevice *core, unsigned int size);
+    public:
+        HWEeprom(AvrDevice *core, unsigned int size);
         void WriteMem(unsigned char *, unsigned int offset, unsigned int size);
-		void Load(const string &filename);
-		virtual	~HWEeprom();
-		void SetEearl(unsigned char);
-		void SetEearh(unsigned char);
-		void SetEedr(unsigned char);
-		void SetEecr(unsigned char);
+        void Load(const string &filename);
+        virtual    ~HWEeprom();
+        void SetEearl(unsigned char);
+        void SetEearh(unsigned char);
+        void SetEedr(unsigned char);
+        void SetEecr(unsigned char);
 
-		unsigned char GetEearl();
-		unsigned char GetEearh();
-		unsigned char GetEecr();
-		unsigned char GetEedr();
-		void WriteAtAddress(unsigned int, unsigned char);
-		unsigned char ReadFromAddress(unsigned int);
+        unsigned char GetEearl();
+        unsigned char GetEearh();
+        unsigned char GetEecr();
+        unsigned char GetEedr();
+        void WriteAtAddress(unsigned int, unsigned char);
+        unsigned char ReadFromAddress(unsigned int);
 
-		virtual unsigned int CpuCycle();
-		void Reset();
+        virtual unsigned int CpuCycle();
+        void Reset();
 
-		enum {
-			READY,
-			READ,
-			WRITE,
-			WRITE_ENABLED
-		} T_State;
+        enum {
+            READY,
+            READ,
+            WRITE,
+            WRITE_ENABLED
+        } T_State;
 
 };
 
@@ -81,7 +81,7 @@ class HWMegaEeprom: public HWEeprom {
         bool irqFlag;
 
     public:
-    HWMegaEeprom(AvrDevice *core, HWIrqSystem *, unsigned int size, unsigned int irqVec); 
+    HWMegaEeprom(AvrDevice *core, HWIrqSystem *, unsigned int size, unsigned int irqVec);
     void SetEecr(unsigned char);
     virtual unsigned int CpuCycle();
     //bool IsIrqFlagSet(unsigned int vector);
@@ -89,37 +89,37 @@ class HWMegaEeprom: public HWEeprom {
 };
 
 class RWEearh: public RWMemoryMembers {
-	protected: 
-		HWEeprom *ee;
-	public:
-		RWEearh(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}; 
-		virtual unsigned char operator=(unsigned char) ;
-		virtual operator unsigned char() const;
+    protected:
+        HWEeprom *ee;
+    public:
+        RWEearh(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){};
+        virtual unsigned char operator=(unsigned char) ;
+        virtual operator unsigned char() const;
 };
 
 class RWEearl: public RWMemoryMembers {
-	protected:
-		HWEeprom *ee;
-	public:
-		RWEearl(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
-		virtual unsigned char operator=(unsigned char) ;
-		virtual operator unsigned char() const;
+    protected:
+        HWEeprom *ee;
+    public:
+        RWEearl(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
+        virtual unsigned char operator=(unsigned char) ;
+        virtual operator unsigned char() const;
 };
 
 class RWEedr: public RWMemoryMembers {
-	protected:
-		HWEeprom *ee;
-	public:
-		RWEedr(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
-		virtual unsigned char operator=(unsigned char) ;
-		virtual operator unsigned char() const;
+    protected:
+        HWEeprom *ee;
+    public:
+        RWEedr(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
+        virtual unsigned char operator=(unsigned char) ;
+        virtual operator unsigned char() const;
 };
 class RWEecr: public RWMemoryMembers {
-	protected:
-		HWEeprom *ee;
-	public:
-		RWEecr(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
-		virtual unsigned char operator=(unsigned char) ;
-		virtual operator unsigned char() const;
+    protected:
+        HWEeprom *ee;
+    public:
+        RWEecr(AvrDevice *c, HWEeprom *e): RWMemoryMembers(c), ee(e){}
+        virtual unsigned char operator=(unsigned char) ;
+        virtual operator unsigned char() const;
 };
 #endif
