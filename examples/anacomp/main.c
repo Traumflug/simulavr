73b55c5c09baa32d19c9a79bfd6dbe7c4825ab98
joelsherrill <joelsherrill>
2009-05-05 14:42:36 +0000

    2009-05-05	Knut Schwichtenberg <kschwi@web.de>
    	* anacomp/main.c: Fix comments and clean up.

--- examples/anacomp/main.c
+++ examples/anacomp/main.c
@@ -1,41 +1,37 @@
-/* show the reading & writing of eeprom memory space
- * also show working interrupts after eeprom write finished in
- * mega devices 
+/* Example of the simulavrxx delivery.
+ * This example shows the usage of the analog comparator
+ * of the AVR mega devices the output of the ersult on Port B.
+ * Compare AIN0 and ANI1. If AIN0 > AIN1 port B=0xff, else port B=0
  *
  * Status: in work
  *
- *  $Id$
+ * $Id$
  */
 
 #include <avr/io.h>
 
-
-volatile unsigned char compReady;
-
-unsigned char results[0x20];
-
-
 int main () {
    int i;
-    DDRB=0xff;
-    volatile int x;
+   volatile int x;     // For future use
+   DDRB=0xff;          // Set all pins of port B to output
+//   SFIOR |= (1<<ACME); // Enable Analog Multiplexer
 
-#if 0
-    for( i=0; i<10000; ++i )
+#ifdef CONNECT_TO_WRITEPIPE
+// To use this feature simulavrxx needs to be connect to
+// a write pipe e.g. by the commandline switch -W0x20,-
+   for( i=0; i<10000; ++i )
        *( (volatile char*) 0x20)='*';
 #endif
-
-    do {
-        if (ACSR & (1<<ACO) ) {
-            PORTB=0xff;
-            x=1;
+//--------------------------------------------
+   do {
+       if (ACSR & (1<<ACO) ) { // Check the result
+          PORTB=0xff;
+          x=1;
         } else {
-           PORTB=0x00;
-            x=0;
+          PORTB=0x00;
+          x=0;
         }
-    } while(1);
-
-    return 0;
+   } while(1);   // do forever
 }
 
 
