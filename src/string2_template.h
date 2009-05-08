e535756349495bdaa0ba5a5a34985d3abb8c4fdc
joelsherrill <joelsherrill>
2009-05-08 19:22:27 +0000

    2009-05-08	Joel Sherrill <joel.sherrill@oarcorp.com>
    	* src/string2_template.h: Add comment block.

--- src/string2_template.h
+++ src/string2_template.h
@@ -1,3 +1,33 @@
+/*
+ ****************************************************************************
+ *
+ * simulavr - A simulator for the Atmel AVR family of microcontrollers.
+ * Copyright (C) 2009   Joel Sherrill
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
+ *
+ *  $Id$
+ */
+
+/*
+ * This file is designed to be included multiple times to instantiate
+ * it and should NOT be protected against multiple inclusions.
+ */
+
 #ifndef STRING_TO_TYPE
   #error "STRING_TO_TYPE not defined"
 #endif
