c0715683bf4f6737006313d88188300c7117d2b8
Onno Kortmann <onno@gmx.net>
2009-07-11 12:45:32 +0200

    Simple SPI master waveform output check in c/verilog
    This small file produces a GTKwave-viewable .vcd file which can be used to
    check the default waveforms of the simulated SPI interface in simulavrxx, at
    the moment only in master mode. It has been written to aid in debugging the
    SPI interface.
    
    TODO: It should probably be extended to cover more stuff of the SPI interface
    and maybe integrated into a future regression test suite.

---
+++ examples/verilog/spi-waveforms.c
@@ -0,0 +1,71 @@
+/*
+** Copyright (C) 2009 Onno Kortmann <onno@gmx.net>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**  
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**  
+*/
+#include <avr/io.h>
+
+#define SPI_CS      2
+#define SPI_MOSI    3
+#define SPI_MISO    4
+#define SPI_SCK     5
+
+void spitxdel() {
+    while(!(SPSR & (1<<SPIF))); SPDR;
+    // 'slave reset'
+    PORTB |= _BV(SPI_CS);
+    PORTB &= ~_BV(SPI_CS);
+}
+
+
+int main() {
+    DDRB |= (1<<SPI_CS);
+    DDRB |= (1<<SPI_MOSI);	
+    DDRB &=~(1<<SPI_MISO);
+    DDRB |= (1<<SPI_SCK);
+
+    // 'slave reset'
+    PORTB |= _BV(SPI_CS);
+    PORTB &= ~_BV(SPI_CS);
+
+    // standard options
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<DORD);
+    SPDR = 0xa5;
+    spitxdel();
+    
+    
+    // two times rate
+    SPSR |= (1<<SPI2X);
+    SPDR = 0xa5;
+    spitxdel();
+    
+    // MSB first
+    SPCR = (1<<SPE)|(1<<MSTR);
+    SPDR = 0xa5;
+    spitxdel();
+
+
+    // inversed clock
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<CPOL);
+    SPDR = 0xa5;
+    spitxdel();
+
+
+    // other phase
+    SPCR = (1<<SPE)|(1<<MSTR)|(1<<CPHA);
+    SPDR = 0xa5;
+    spitxdel();
+}
---
+++ examples/verilog/spi-waveforms.v
@@ -0,0 +1,51 @@
+/*
+** Copyright (C) 2009 Onno Kortmann <onno@gmx.net>
+**  
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation; either version 2 of the License, or
+** (at your option) any later version.
+**  
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**  
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+**  
+*/
+/* SPI slave Verilog example code. */
+`timescale 1ns / 1ns
+
+module test;
+   reg CLK;
+   wire nCS, SCK, MOSI;
+   wire MISO;
+
+   wire [7:0] pb;
+   wire [7:0] pc;
+   wire [7:0] pd;
+
+   assign    MISO=pb[4];
+   assign    MOSI=pb[3];
+   assign    SCK=pb[5];
+   assign    nCS=pb[2];
+   
+   defparam  avr.progfile="spi-waveforms.elf";
+   ATmega8 avr(CLK, pb, pc, pd);
+   
+   initial begin
+      $avr_trace("spi-waveforms.trace");
+      $dumpfile("spi-waveforms.vcd");
+      $dumpvars(0, test);
+      # 100000 $finish;
+   end // initial begin
+   
+   always begin
+      #100 CLK<=0; 
+      #100 CLK<=1;
+   end   
+endmodule
+   
