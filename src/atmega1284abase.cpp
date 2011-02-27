b1bce0b928b227090f95c80208bfddb379190d98
Petr Hluzin <adgrer@sdfgsdfgh.invalid>
2011-02-27 02:27:45 +0100

    Added copies of atmega668base.{c,h} files. This will make tracking of future differences easier.

---
+++ src/atmega1284abase.cpp
@@ -0,0 +1,276 @@
+ /*
+ ****************************************************************************
+ *
+ * simulavr - A simulator for the Atmel AVR family of microcontrollers.
+ * Copyright (C) 2001, 2002, 2003   Klaus Rudolph       
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
+ */
+#include "atmega668base.h"
+
+#include "irqsystem.h"
+#include "hwstack.h"
+#include "hweeprom.h"
+#include "hwwado.h"
+#include "hwsreg.h"
+#include "avrerror.h"
+#include "avrfactory.h"
+
+AVR_REGISTER(atmega48, AvrDevice_atmega48);
+AVR_REGISTER(atmega88, AvrDevice_atmega88);
+AVR_REGISTER(atmega168, AvrDevice_atmega168);
+AVR_REGISTER(atmega328, AvrDevice_atmega328);
+
+AvrDevice_atmega668base::~AvrDevice_atmega668base() {
+    delete usart0;
+    delete wado;
+    delete spi;
+    delete ad;
+    delete timer2;
+    delete timerIrq2;
+    delete timer1;
+    delete inputCapture1;
+    delete timerIrq1;
+    delete timer0;
+    delete timerIrq0;
+    delete extirqpc;
+    delete pcmsk2_reg;
+    delete pcmsk1_reg;
+    delete pcmsk0_reg;
+    delete pcifr_reg;
+    delete pcicr_reg;
+    delete extirq01;
+    delete eifr_reg;
+    delete eimsk_reg;
+    delete eicra_reg;
+    delete stack;
+    delete eeprom;
+    delete irqSystem;
+}
+
+AvrDevice_atmega668base::AvrDevice_atmega668base(unsigned ram_bytes,
+                                                 unsigned flash_bytes,
+                                                 unsigned ee_bytes ):
+    AvrDevice(224,          // I/O space above General Purpose Registers
+              ram_bytes,    // RAM size
+              0,            // External RAM size
+              flash_bytes), // Flash Size
+    aref(),
+    adc6(),
+    adc7(),
+    portb(this, "B", true),
+    portc(this, "C", true, 7),
+    portd(this, "D", true),
+    assr_reg(&coreTraceGroup, "ASSR"),
+    gtccr_reg(&coreTraceGroup, "GTCCR"),
+    prescaler01(this, "01", &gtccr_reg, 0, 7),
+    prescaler2(this, "2", PinAtPort(&portb, 6), &assr_reg, 5, &gtccr_reg, 1, 7),
+    admux(this,
+          &portc.GetPin(0),
+          &portc.GetPin(1),
+          &portc.GetPin(2),
+          &portc.GetPin(3),
+          &portc.GetPin(4),
+          &portc.GetPin(5),
+          &adc6,
+          &adc7)
+{ 
+    flagJMPInstructions = (flash_bytes > 8U * 1024U) ? true : false;
+    irqSystem = new HWIrqSystem(this, (flash_bytes > 8U * 1024U) ? 4 : 2, 26);
+    
+    eeprom = new HWEeprom(this, irqSystem, ee_bytes, 23, HWEeprom::DEVMODE_EXTENDED); 
+    stack = new HWStackSram(this, 16);
+
+    RegisterPin("AREF", &aref);
+    RegisterPin("ADC6", &adc6);
+    RegisterPin("ADC7", &adc7);
+
+    eicra_reg = new IOSpecialReg(&coreTraceGroup, "EICRA");
+    eimsk_reg = new IOSpecialReg(&coreTraceGroup, "EIMSK");
+    eifr_reg = new IOSpecialReg(&coreTraceGroup, "EIFR");
+    extirq01 = new ExternalIRQHandler(this, irqSystem, eimsk_reg, eifr_reg);
+    extirq01->registerIrq(1, 0, new ExternalIRQSingle(eicra_reg, 0, 2, GetPin("D2")));
+    extirq01->registerIrq(2, 1, new ExternalIRQSingle(eicra_reg, 2, 2, GetPin("D3")));
+
+    pcicr_reg = new IOSpecialReg(&coreTraceGroup, "PCICR");
+    pcifr_reg = new IOSpecialReg(&coreTraceGroup, "PCIFR");
+    pcmsk0_reg = new IOSpecialReg(&coreTraceGroup, "PCMSK0");
+    pcmsk1_reg = new IOSpecialReg(&coreTraceGroup, "PCMSK1");
+    pcmsk2_reg = new IOSpecialReg(&coreTraceGroup, "PCMSK2");
+    extirqpc = new ExternalIRQHandler(this, irqSystem, pcicr_reg, pcifr_reg);
+    extirqpc->registerIrq(3, 0, new ExternalIRQPort(pcmsk0_reg, &portb));
+    extirqpc->registerIrq(4, 1, new ExternalIRQPort(pcmsk1_reg, &portc));
+    extirqpc->registerIrq(5, 2, new ExternalIRQPort(pcmsk2_reg, &portd));
+    
+    timerIrq0 = new TimerIRQRegister(this, irqSystem, 0);
+    timerIrq0->registerLine(0, new IRQLine("TOV0",  16));
+    timerIrq0->registerLine(1, new IRQLine("OCF0A", 14));
+    timerIrq0->registerLine(2, new IRQLine("OCF0B", 15));
+    
+    timer0 = new HWTimer8_2C(this,
+                             new PrescalerMultiplexerExt(&prescaler01, PinAtPort(&portd, 4)),
+                             0,
+                             timerIrq0->getLine("TOV0"),
+                             timerIrq0->getLine("OCF0A"),
+                             new PinAtPort(&portd, 6),
+                             timerIrq0->getLine("OCF0B"),
+                             new PinAtPort(&portd, 5));
+
+    timerIrq1 = new TimerIRQRegister(this, irqSystem, 1);
+    timerIrq1->registerLine(0, new IRQLine("TOV1",  13));
+    timerIrq1->registerLine(1, new IRQLine("OCF1A", 11));
+    timerIrq1->registerLine(2, new IRQLine("OCF1B", 12));
+    timerIrq1->registerLine(5, new IRQLine("ICF1",  10));
+    
+    inputCapture1 = new ICaptureSource(PinAtPort(&portb, 0));
+    timer1 = new HWTimer16_2C3(this,
+                               new PrescalerMultiplexerExt(&prescaler01, PinAtPort(&portd, 5)),
+                               1,
+                               timerIrq1->getLine("TOV1"),
+                               timerIrq1->getLine("OCF1A"),
+                               new PinAtPort(&portb, 1),
+                               timerIrq1->getLine("OCF1B"),
+                               new PinAtPort(&portb, 2),
+                               timerIrq1->getLine("ICF1"),
+                               inputCapture1);
+    
+    timerIrq2 = new TimerIRQRegister(this, irqSystem, 2);
+    timerIrq2->registerLine(0, new IRQLine("TOV2",  9));
+    timerIrq2->registerLine(1, new IRQLine("OCF2A", 7));
+    timerIrq2->registerLine(2, new IRQLine("OCF2B", 8));
+    
+    timer2 = new HWTimer8_2C(this,
+                             new PrescalerMultiplexer(&prescaler2),
+                             2,
+                             timerIrq2->getLine("TOV2"),
+                             timerIrq2->getLine("OCF2A"),
+                             new PinAtPort(&portb, 3),
+                             timerIrq2->getLine("OCF2B"),
+                             new PinAtPort(&portd, 3));
+
+    ad = new HWAd(this, &admux, irqSystem, aref, 21);
+    spi = new HWSpi(this,
+                    irqSystem,
+                    PinAtPort(&portb, 3),   // MOSI
+                    PinAtPort(&portb, 4),   // MISO
+                    PinAtPort(&portb, 5),   // SCK
+                    PinAtPort(&portb, 2),   // /SS
+                    17,                     // irqvec
+                    true);
+    
+    wado = new HWWado(this);
+
+    usart0 = new HWUsart(this,
+                         irqSystem,
+                         PinAtPort(&portd,1),    // TXD
+                         PinAtPort(&portd,0),    // RXD
+                         PinAtPort(&portd, 4),   // XCK
+                         19,   // (18) RX complete vector
+                         20,   // (19) UDRE vector
+                         21);  // (20) TX complete vector
+
+    rw[0xE6]= & usart0->udr_reg;
+
+    rw[0xE4]= & usart0->ubrr_reg;
+
+    rw[0xE1]= & usart0->ucsrb_reg;
+    rw[0xE0]= & usart0->ucsra_reg;
+
+    rw[0xC5]= & usart0->ubrrhi_reg;
+
+    rw[0xC2]= & usart0->ucsrc_reg;
+
+    rw[0xb6]= & assr_reg;
+    // 0xb5 reserved
+    rw[0xb4]= & timer2->ocrb_reg;
+    rw[0xb3]= & timer2->ocra_reg;
+    rw[0xb2]= & timer2->tcnt_reg;
+    rw[0xb1]= & timer2->tccrb_reg;
+    rw[0xb0]= & timer2->tccra_reg;
+    // 0x8c - 0xaf reserved
+    rw[0x8b]= & timer1->ocrb_h_reg;
+    rw[0x8a]= & timer1->ocrb_l_reg;
+    rw[0x89]= & timer1->ocra_h_reg;
+    rw[0x88]= & timer1->ocra_l_reg;
+    rw[0x87]= & timer1->icr_h_reg;
+    rw[0x86]= & timer1->icr_l_reg;
+    rw[0x85]= & timer1->tcnt_h_reg;
+    rw[0x84]= & timer1->tcnt_l_reg;
+    // 0x83 reserved
+    rw[0x82]= & timer1->tccrc_reg;
+    rw[0x81]= & timer1->tccrb_reg;
+    rw[0x80]= & timer1->tccra_reg;
+    
+    rw[0x7C]= & admux.admux_reg;
+
+    rw[0x7A]= & ad->adcsr_reg;
+    rw[0x79]= & ad->adch_reg;
+    rw[0x78]= & ad->adcl_reg;
+
+    rw[0x70]= & timerIrq2->timsk_reg;
+    rw[0x6F]= & timerIrq1->timsk_reg;
+    rw[0x6E]= & timerIrq0->timsk_reg;
+
+    rw[0x6d]= pcmsk2_reg;
+    rw[0x6c]= pcmsk1_reg;
+    rw[0x6b]= pcmsk0_reg;
+    
+    rw[0x69]= eicra_reg;
+    rw[0x68]= pcicr_reg;
+
+    rw[0x5f]= statusRegister;
+    rw[0x5e]= & ((HWStackSram *)stack)->sph_reg;
+    rw[0x5d]= & ((HWStackSram *)stack)->spl_reg;
+
+    rw[0x4E]= & spi->spdr_reg;
+    rw[0x4D]= & spi->spsr_reg;
+    rw[0x4C]= & spi->spcr_reg;
+
+    rw[0x48]= & timer0->ocrb_reg;
+    rw[0x47]= & timer0->ocra_reg;
+    rw[0x46]= & timer0->tcnt_reg;
+    rw[0x45]= & timer0->tccrb_reg;
+    rw[0x44]= & timer0->tccra_reg;
+    rw[0x43]= & gtccr_reg;
+    rw[0x42]= & eeprom->eearh_reg;
+    rw[0x41]= & eeprom->eearl_reg;
+    rw[0x40]= & eeprom->eedr_reg;
+    rw[0x3F]= & eeprom->eecr_reg;
+
+    rw[0x3D]= eimsk_reg;
+    rw[0x3C]= eifr_reg;
+    rw[0x3b]= pcifr_reg;
+
+    rw[0x37]= & timerIrq2->tifr_reg;
+    rw[0x36]= & timerIrq1->tifr_reg;
+    rw[0x35]= & timerIrq0->tifr_reg;
+
+    rw[0x2B]= & portd.port_reg;
+    rw[0x2A]= & portd.ddr_reg;
+    rw[0x29]= & portd.pin_reg;
+    
+    rw[0x28]= & portc.port_reg;
+    rw[0x27]= & portc.ddr_reg;
+    rw[0x26]= & portc.pin_reg;
+    
+    rw[0x25]= & portb.port_reg;
+    rw[0x24]= & portb.ddr_reg;
+    rw[0x23]= & portb.pin_reg;
+
+    Reset();
+}
+
---
+++ src/atmega1284abase.h
@@ -0,0 +1,117 @@
+ /*
+ ****************************************************************************
+ *
+ * simulavr - A simulator for the Atmel AVR family of microcontrollers.
+ * Copyright (C) 2001, 2002, 2003   Klaus Rudolph       
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ ****************************************************************************
+ */
+#ifndef ATMEGA668
+#define ATMEGA668
+
+#include "avrdevice.h"
+#include "hardware.h"
+#include "rwmem.h"
+#include "externalirq.h"
+#include "hwuart.h"
+#include "hwad.h"
+#include "hwport.h"
+#include "hwspi.h"
+#include "hwtimer/timerprescaler.h"
+#include "hwtimer/hwtimer.h"
+
+/*! AVRDevice class for ATMega48/88/168/328
+  \todo This device isn't completely implemented. There is no
+  boot loader section support for >= ATMega88, only normal interrupt vector
+  start address supported, incomplete usart registers (and maybe more ...) */
+class AvrDevice_atmega668base: public AvrDevice {
+    
+    protected:
+        Pin                 aref;        //!< analog reference pin
+        Pin                 adc6;        //!< adc channel 6 input pin
+        Pin                 adc7;        //!< adc channel 7 input pin
+        HWPort              portb;       //!< port B
+        HWPort              portc;       //!< port C
+        HWPort              portd;       //!< port D
+        IOSpecialReg        gtccr_reg;   //!< GTCCR IO register
+        IOSpecialReg        assr_reg;    //!< ASSR IO register
+        HWPrescaler         prescaler01; //!< prescaler unit for timer 0 and 1
+        HWPrescalerAsync    prescaler2;  //!< prescaler unit for timer 2
+        ExternalIRQHandler* extirq01;    //!< external interrupt support for INT0, INT1
+        IOSpecialReg*       eicra_reg;   //!< EICRA IO register
+        IOSpecialReg*       eimsk_reg;   //!< EIMSK IO register
+        IOSpecialReg*       eifr_reg;    //!< EIFR IO register
+        ExternalIRQHandler* extirqpc;    //!< external interrupt support for PCINT[0-2]
+        IOSpecialReg*       pcicr_reg;   //!< PCICR IO register
+        IOSpecialReg*       pcifr_reg;   //!< PCIFR IO register
+        IOSpecialReg*       pcmsk0_reg;  //!< PCIMSK0 IO register
+        IOSpecialReg*       pcmsk1_reg;  //!< PCIMSK1 IO register
+        IOSpecialReg*       pcmsk2_reg;  //!< PCIMSK2 IO register
+        HWAdmux             admux;       //!< adc multiplexer unit
+        HWAd*               ad;          //!< adc unit
+        HWSpi*              spi;         //!< spi unit
+        HWUsart*            usart0;      //!< usart 0 unit
+        TimerIRQRegister*   timerIrq0;   //!< timer interrupt unit for timer 0
+        HWTimer8_2C*        timer0;      //!< timer 0 unit
+        ICaptureSource*     inputCapture1; //!< input capture source for timer1
+        TimerIRQRegister*   timerIrq1;   //!< timer interrupt unit for timer 1
+        HWTimer16_2C3*      timer1;      //!< timer 1 unit
+        TimerIRQRegister*   timerIrq2;   //!< timer interrupt unit for timer 2
+        HWTimer8_2C*        timer2;      //!< timer 2 unit
+        
+    public:
+        
+        /*! Creates the device for ATMega48/88/168/328
+          @param ram_bytes how much SRAM does the device own
+          @param flash_bytes how much flash memory does the device own
+          @param ee_bytes how much EEPROM space does the device own */
+        AvrDevice_atmega668base(unsigned ram_bytes, unsigned flash_bytes,
+                                unsigned ee_bytes );
+        
+        ~AvrDevice_atmega668base();
+        
+};
+
+//! AVR device class for ATMega328, see AvrDevice_atmega668base.
+class AvrDevice_atmega328: public AvrDevice_atmega668base {
+    public:
+        //! Creates the device for ATMega328, see AvrDevice_atmega668base.
+        AvrDevice_atmega328() : AvrDevice_atmega668base(2 * 1024, 32 * 1024, 1024) {}
+};
+
+//! AVR device class for ATMega168, see AvrDevice_atmega668base.
+class AvrDevice_atmega168: public AvrDevice_atmega668base {
+    public:
+        //! Creates the device for ATMega168, see AvrDevice_atmega668base.
+        AvrDevice_atmega168() : AvrDevice_atmega668base(1024, 16 * 1024, 512) {}
+};
+
+//! AVR device class for ATMega88, see AvrDevice_atmega668base.
+class AvrDevice_atmega88:public AvrDevice_atmega668base {
+    public:
+        //! Creates the device for ATMega88, see AvrDevice_atmega668base.
+        AvrDevice_atmega88() : AvrDevice_atmega668base(1024, 8 * 1024, 512) {}
+};
+
+//! AVR device class for ATMega48, see AvrDevice_atmega668base.
+class AvrDevice_atmega48:public AvrDevice_atmega668base {
+    public:
+        //! Creates the device for ATMega48, see AvrDevice_atmega668base.
+        AvrDevice_atmega48() : AvrDevice_atmega668base(512, 4 * 1024, 256) {}
+};
+
+#endif
