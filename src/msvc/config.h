518b03ea02f4c7d5ec90b722c7f0683808afc915
Petr Hluzin <adgrer@sdfgsdfgh.invalid>
2011-03-16 23:19:49 +0100

    Failed to build on VS2008.

---
+++ src/msvc/config.h
@@ -0,0 +1,11 @@
+
+#ifndef _MSC_VER // [
+#  error "Use this header only with Microsoft Visual C++ compilers!"
+#endif // _MSC_VER ]
+
+/* This file is loosely based on src/config.h generated on
+ * my Cygwin/MinGW/whatever system. Simulavr does not care
+ * about the other #define's. */
+
+/* Define to 1 if you build simulavr on windows with MSYS/MingW */
+/* #undef HAVE_SYS_MINGW */
